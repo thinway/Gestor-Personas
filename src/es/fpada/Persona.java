7146e67960370582dbd3c6b09efbb1c614bbd1fa
Fran Thinway <thinway@icloud.com>
2015-01-29 11:02:48 +0100

    Persona's accessor methods.

--- src/es/fpada/Persona.java
+++ src/es/fpada/Persona.java
@@ -10,4 +10,45 @@ public class Persona {
     private char sexo;
     private double peso;
     private double altura;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 }
