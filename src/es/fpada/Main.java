d39cb4c29a7ffd7e8b4f0f475bf089e2e8ed6a37
Fran Thinway <thinway@icloud.com>
2015-01-29 11:00:13 +0100

    Created Persona Class.

---
+++ src/es/fpada/Main.java
@@ -0,0 +1,8 @@
+package es.fpada;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    
+    }
+}
---
+++ src/es/fpada/Persona.java
@@ -0,0 +1,8 @@
+package es.fpada;
+
+/**
+ * Created by Fran Delgado on 29/1/15.
+ */
+public class Persona {
+    
+}
